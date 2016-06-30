bfc51b9d2f2a29fc760f00d0d54e6fd43c977bf0
Kim Wong <kim.wong@st-lenovo-t440p.scilifelab.se>
2016-06-30 14:40:47 +0200

    The client-side 'uploading' of images works through an imageUploader directive associated with the buttonBar module

--- st_alignment/js/button-bar/button-bar.component.js
+++ st_alignment/js/button-bar/button-bar.component.js
@@ -3,6 +3,38 @@
 angular.module('buttonBar')
     .component('buttonBar', {
         templateUrl: 'templates/button-bar.template.html',
-        controller: function() {
-        }
+        controller: [
+            '$scope',
+            function($scope) {
+                $scope.poop = 123;
+                $scope.$on('imageSelected', function(event, args) {
+                    /* run on the selection of a new image */
+                    $scope.$apply(function() {
+                        $scope.imageURL = args.imageURL;
+                    });
+                });
+            }
+        ]
+    })
+    .directive('imageUpload', function() {
+    /* detects the selection of an image upon pressing the upload button
+     * and using the FileReader, converts the image data into a data URL,
+     * which $emits a message upon loading and reading the result */
+        return {
+            scope: true,
+            link: function(scope, elem, attrs) {
+                elem.bind('change', function(event) {
+                    var img = event.target.files[0];
+                    var reader = new FileReader();
+                    reader.addEventListener('load', function() {
+                        scope.$emit('imageSelected', {
+                            imageURL: reader.result
+                        });
+                    }, false);
+                    if(img) {
+                        reader.readAsDataURL(img);
+                    }
+                });
+            }
+        };
     });
