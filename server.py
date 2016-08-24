2689df506e9e7dd179b7fe01f8bfd3f08b057a47
Kim <kimspindel@gmail.com>
2016-08-24 11:43:26 +0200

    Server has been ported to Python 2.7 instead of Python 3. A create_spots_from_keypoints() function has been added to the Spots() class, ready to receive keypoint data from the cv2.SimpleBlobDetector() class.

--- server.py
+++ server.py
@@ -1,31 +1,93 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import base64
 import io
 import json
+import math
 import time
 from bottle import error, get, post, response, request, route, run, static_file
 from PIL import Image
+
+def distance_between(a, b):
+    w = a['x'] - b['x']
+    h = a['y'] - b['y']
+    dist = math.sqrt(w * w + h * h)
+    return dist
 
 class Spots:
     """Holds the spot data"""
     spots = []
 
+    array_size = {'x': 33, 'y': 35}
+    TL_coords = {
+        'x': 1251,
+        'y': 676
+    }
+    BR_coords = {
+        'x': 11780, 
+        'y': 11982
+    }
+    spacer = {'x': 330, 'y': 333};
+
+    def get_spots(self):
+        spot_dictionary = {'spots': self.spots}
+        return spot_dictionary
+        
     def create_spots(self):
-        """temporary function manually creates some 'arbitrary' spots"""
-        arrayWidth = 33;
-        arrayHeight = 35;
-        spacer = {'x': 330, 'y': 333};
-        offset = {'x': 5100, 'y': 4730};
-        for i in range(0, arrayHeight):
-            for j in range(0, arrayWidth):
+        """Temporary function manually creates some 'arbitrary' spots"""
+        self.spacer = {'x': 330, 'y': 333};
+        self.TL_coords = {'x': 5100, 'y': 4730};
+        for i in range(0, array_size['x']):
+            for j in range(0, array_size['y']):
                 self.spots.append({
                     'arrayPosition': {'x': j + 1, 'y': i + 1},
-                    'renderPosition': {'x': spacer['x'] * j + offset['x'],
-                                       'y': spacer['y'] * i + offset['y']},
+                    'renderPosition': {'x': self.spacer['x'] * j + self.TL_coords['x'],
+                                       'y': self.spacer['y'] * i + self.TL_coords['y']},
                     'selected': False,
                     'size': 90
-                });
+                })
+        spot_dictionary = {'spots': self.spots}
+        return spot_dictionary
+
+    def set_array_size(self, size):
+        self.array_size = size
+
+    def set_coords(self, TL, BR):
+        self.TL_coords = TL
+        self.BR_coords = BR
+        self.spacer = {
+            'x': (self.BR_coords['x'] - self.TL_coords['x'])
+                 / (self.array_size['x'] - 1),
+            'y': (self.BR_coords['y'] - self.TL_coords['y'])
+                 / (self.array_size['y'] - 1)
+        }
+
+    def create_spots_from_keypoints(self, keypoints):
+        """Takes keypoints generated from opencv and tries to match them to
+        their correct array coordinate.
+        An array of spots wrapped in a dictionary is returned.
+        """
+        threshold_distance = 80
+        for i in range(0, self.array_size['x']):
+            for j in range(0, self.array_size['y']):
+                predicted_position = {'x': self.spacer['x'] * j + self.TL_coords['x'],
+                                      'y': self.spacer['y'] * i + self.TL_coords['y']
+                }
+                for kp in keypoints:
+                    kp_position = {
+                        'x': kp.pt[0],
+                        'y': kp.pt[1]
+                    }
+                    if(distance_between(kp_position, predicted_position) < threshold_distance):
+                        self.spots.append({
+                            'arrayPosition': {'x': j + 1, 'y': i + 1},
+                            'renderPosition': {'x': kp_position['x'],
+                                               'y': kp_position['y']
+                            },
+                            'selected': False,
+                            'size': 90
+                        })
         spot_dictionary = {'spots': self.spots}
         return spot_dictionary
 
@@ -49,6 +111,7 @@ class Tilemap:
 
 class ImageProcessor:
     """Takes the jpeg image and performs various methods on it."""
+    # look into implementing this https://www.learnopencv.com/blob-detection-using-opencv-python-c/
     URI_header = b'data:image/jpeg;base64,'
 
     def validate_jpeg_URI(self, jpeg_URI):
@@ -71,11 +134,11 @@ class ImageProcessor:
         image.save(jpeg_data, 'JPEG')
 
         # encode the data into a URI with the header added
-        jpeg_string = base64.b64encode(jpeg_data.getbuffer())
+        jpeg_string = base64.b64encode(jpeg_data.getvalue())
         jpeg_string = self.URI_header + jpeg_string
 
         # convert the string from a raw literal to utf-8 encoding
-        jpeg_string = str(jpeg_string, 'utf-8')
+        jpeg_string = unicode(jpeg_string, 'utf-8')
         return jpeg_string
 
 
@@ -160,7 +223,7 @@ image_processor = ImageProcessor()
 
 @get('/spots')
 def get_spots():
-    return spots.create_spots()
+    return spots.get_spots()
     
 @get('/tiles')
 def get_tiles():
@@ -183,8 +246,8 @@ def receive_image(filepath):
         timer_start = time.time()
         my_image = image_processor.jpeg_URI_to_Image(image_string)
         for x in tiles.dict_wrapper['tilemapLevels']:
-            #tiles.put_tiles_at(x, image_processor.tile_image(my_image, x))
-            tiles.put_tiles_at(x, image_processor.tile_image_dummy(my_image, 20))
+            tiles.put_tiles_at(x, image_processor.tile_image(my_image, x))
+            #tiles.put_tiles_at(x, image_processor.tile_image_dummy(my_image, 20))
 
         print(time.time() - timer_start)
         return
