f3d471f7ee07dc33b24955fdec9d6dd3549ea78d
Kim <kimspindel@gmail.com>
2016-09-23 13:16:48 +0200

    Canvas now resizes to roughly the size of the browser window

--- st_alignment/js/viewer/viewer.component.js
+++ st_alignment/js/viewer/viewer.component.js
@@ -6,5 +6,18 @@ angular.module('viewer')
             '$rootScope',
             function($scope, $rootScope) {
                 $scope.imageLoaded = false; 
+                console.log("width " + window.innerWidth);
+                console.log("height " + window.innerHeight);
+                // this code is to size the canvas to roughly the size of the window.
+                // it is a bit of a hack and there should be a better way to do it.
+                document.getElementById('my-canvas').width  = window.innerWidth - 60;
+                document.getElementById('my-canvas').height = window.innerHeight - 100;
+                window.addEventListener('resize', function(event) 
+                {
+                    console.log("width " + window.innerWidth);
+                    console.log("height " + window.innerHeight);
+                    document.getElementById('my-canvas').width  = window.innerWidth - 60;
+                    document.getElementById('my-canvas').height = window.innerHeight - 100;
+                });
             }]
     });
