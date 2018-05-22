c45b810ff6715dd5617d792bc67b5ad38564ece1
Ludvig Bergenstråhle <ludvig.bergenstrahle@scilifelab.se>
2018-05-22 13:36:39 +0200

    Set number of workers according to CPU affinity

--- server/app/__main__.py
+++ server/app/__main__.py
@@ -1,6 +1,8 @@
 """ Runs the web server
 """
 
+from os import sched_getaffinity
+
 from . import application
 
-application.run(host='0.0.0.0', port=8080)
+application.run(host='0.0.0.0', port=8080, workers=len(sched_getaffinity(0)))
